f0214b926bffb711529066506a17080a79a49cee
Tessier Kevin <kevin-tessier@protonmail.com>
2026-05-25 11:07:00 +0200

    add : add patial themejson

--- web/app/themes/sage/vite.config.js
+++ web/app/themes/sage/vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
-import laravel from 'laravel-vite-plugin'
+import laravel from 'laravel-vite-plugin';
 import { wordpressPlugin, wordpressThemeJson } from '@roots/vite-plugin';
 
 // Set APP_URL if it doesn't exist for Laravel Vite plugin
-if (! process.env.APP_URL) {
+if (!process.env.APP_URL) {
   process.env.APP_URL = 'http://example.test';
 }
 
@@ -28,6 +28,7 @@ export default defineConfig({
     // Generate the theme.json file in the public/build/assets directory
     // based on the Tailwind config and the theme.json file from base theme folder
     wordpressThemeJson({
+      partials: ['resources/views/blocks'],
       disableTailwindColors: false,
       disableTailwindFonts: false,
       disableTailwindFontSizes: false,
@@ -42,4 +43,7 @@ export default defineConfig({
       '@images': '/resources/images',
     },
   },
-})
+  build: {
+    minify: 'terser',
+  },
+});
