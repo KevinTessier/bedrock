fb85c21829a7348f808afba5f834e4ff7f129103
Tessier Kevin <kevin-tessier@protonmail.com>
2026-05-25 11:06:13 +0200

    write

---
+++ web/app/themes/sage/resources/views/blocks/_global.theme.js
@@ -0,0 +1,15 @@
+export default {
+  blocks: {
+    'core/paragraph': {
+      spacing: { margin: { bottom: '1rem' } },
+    },
+  },
+  elements: {
+    h1: {
+      typography: {
+        fontSize: 'var(--wp--preset--font-size--4-xl)',
+        fontWeight: '600',
+      },
+    },
+  },
+};
